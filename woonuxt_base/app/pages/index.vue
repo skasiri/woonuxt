3770eb53c0545a6f79d08111ed4b35290fed1d5e
Saeid Kasiri <s.k.flower@gmail.com>
2025-09-22 19:59:54 +0330

    text-primary

--- woonuxt_base/app/pages/index.vue
+++ woonuxt_base/app/pages/index.vue
@@ -32,38 +32,38 @@ useSeoMeta({
     </div>
 
     <section class="container my-16">
-      <div class="flex items-end justify-between text-[#C1C6E3]">
+      <div class="flex items-end justify-between text-primary">
         <h2 class="text-lg font-semibold md:text-2xl">{{ $t('messages.shop.shopByCategory') }}</h2>
         <NuxtLink class="text-primary" to="/categories">{{ $t('messages.general.viewAll') }}</NuxtLink>
       </div>
-      <div class="grid justify-center grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-6 text-[#C1C6E3]">
+      <div class="grid justify-center grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-6 text-primary">
         <CategoryCard v-for="(category, i) in productCategories" :key="i" class="w-full" :node="category" />
       </div>
     </section>
 
     <section class="container grid gap-4 my-24 md:grid-cols-2 lg:grid-cols-4">
-      <div class="flex items-center gap-8 p-8 bg-[#060b2f] text-[#C1C6E3] rounded-lg">
+      <div class="flex items-center gap-8 p-8 bg-[#060b2f] text-primary rounded-lg">
         <img src="/icons/box.svg" width="60" height="60" alt="Free Shipping" loading="lazy" />
         <div>
           <h3 class="text-xl font-semibold">Free Shipping</h3>
           <p class="text-sm">Free shipping on order over €50</p>
         </div>
       </div>
-      <div class="flex items-center gap-8 p-8 bg-[#060b2f] text-[#C1C6E3] rounded-lg">
+      <div class="flex items-center gap-8 p-8 bg-[#060b2f] text-primary rounded-lg">
         <img src="/icons/moneyback.svg" width="60" height="60" alt="Money Back" loading="lazy" />
         <div>
           <h3 class="text-xl font-semibold">Peace of Mind</h3>
           <p class="text-sm">30 days money back guarantee</p>
         </div>
       </div>
-      <div class="flex items-center gap-8 p-8 bg-[#060b2f] text-[#C1C6E3] rounded-lg">
+      <div class="flex items-center gap-8 p-8 bg-[#060b2f] text-primary rounded-lg">
         <img src="/icons/secure.svg" width="60" height="60" alt="Secure Payment" loading="lazy" />
         <div>
           <h3 class="text-xl font-semibold">100% Payment Secure</h3>
           <p class="text-sm">Your payment are safe with us.</p>
         </div>
       </div>
-      <div class="flex items-center gap-8 p-8 bg-[#060b2f] text-[#C1C6E3] rounded-lg">
+      <div class="flex items-center gap-8 p-8 bg-[#060b2f] text-primary rounded-lg">
         <img src="/icons/support.svg" width="60" height="60" alt="Support 24/7" loading="lazy" />
         <div>
           <h3 class="text-xl font-semibold">Support 24/7</h3>
@@ -74,8 +74,8 @@ useSeoMeta({
 
     <section class="container my-16" v-if="popularProducts">
       <div class="flex items-end justify-between">
-        <h2 class="text-lg font-semibold md:text-2xl text-[#C1C6E3]">{{ $t('messages.shop.popularProducts') }}</h2>
-        <NuxtLink class="text-[#C1C6E3]" to="/products">{{ $t('messages.general.viewAll') }}</NuxtLink>
+        <h2 class="text-lg font-semibold md:text-2xl text-primary">{{ $t('messages.shop.popularProducts') }}</h2>
+        <NuxtLink class="text-primary" to="/products">{{ $t('messages.general.viewAll') }}</NuxtLink>
       </div>
       <ProductRow :products="popularProducts" class="grid-cols-2 md:grid-cols-4 lg:grid-cols-5 mt-8" />
     </section>
