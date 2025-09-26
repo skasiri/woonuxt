d8c4250a4f4053669491f710ebcfcb1494c48a79
Saeid Kasiri <s.k.flower@gmail.com>
2025-09-26 14:33:20 +0330

    setLocale

--- woonuxt_base/app/components/LangSwitcher.vue
+++ woonuxt_base/app/components/LangSwitcher.vue
@@ -1,13 +1,16 @@
 <script setup>
-const { locales, locale, setLocaleCookie } = useI18n();
+const { locales, locale, setLocale, setLocaleCookie } = useI18n();
 
-watch(locale, (newLocale) => {
-  if (newLocale) setLocaleCookie(newLocale);
+watch(locale, async (newLocale, oldLocale) => {
+  if (newLocale && newLocale !== oldLocale) {
+    await setLocale(newLocale);
+    setLocaleCookie(newLocale);
+  }
 });
 </script>
 
 <template>
   <select id="language-switcher" v-model="locale" aria-label="Language switcher" class="bg-white">
-    <option v-for="locale in locales" :key="locale" :value="locale.code" v-html="locale.name" />
+    <option v-for="l in locales" :key="l.code" :value="l.code" v-html="l.name" />
   </select>
 </template>
