41f633998c8f40414f937127ee88e50da77f1d94
Saeid Kasiri <s.k.flower@gmail.com>
2025-09-26 16:09:02 +0330

    setLocale

---
+++ woonuxt_base/app/pages/[lang]/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const { locales, setLocale, t } = useI18n();
+const route = useRoute();
+
+const lang = route.params.lang as string;
+
+const exists = locales.value.some((l) => l.code === lang);
+
+if (!exists) {
+  throw showError({ statusCode: 404, statusMessage: t('messages.general.languageNotFound') });
+}
+
+setLocale(lang as any);
+</script>
+
+<template>
+  <HomePage />
+</template>
