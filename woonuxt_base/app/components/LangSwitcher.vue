3356d0a4b6c9145bd00c2ed0be15a66358075e19
Saeid Kasiri <s.k.flower@gmail.com>
2025-09-26 23:28:39 +0330

    switchLocalePath

--- woonuxt_base/app/components/LangSwitcher.vue
+++ woonuxt_base/app/components/LangSwitcher.vue
@@ -1,16 +1,29 @@
 <script setup>
-const { locales, locale, setLocale, setLocaleCookie } = useI18n();
+const { locales, setLocaleCookie } = useI18n();
+const switchLocalePath = useSwitchLocalePath();
+const router = useRouter();
 
-watch(locale, async (newLocale, oldLocale) => {
-  if (newLocale && newLocale !== oldLocale) {
-    await setLocale(newLocale);
-    setLocaleCookie(newLocale);
-  }
-});
+// watch(locale, async (newLocale, oldLocale) => {
+//   if (newLocale && newLocale !== oldLocale) {
+//     await setLocale(newLocale);
+//     setLocaleCookie(newLocale);
+//   }
+// });
+
+function onChange(event) {
+  const newCode = event.target.value;
+  setLocaleCookie(newCode);
+  const path = switchLocalePath(newCode);
+  router.push(path);
+}
 </script>
 
 <template>
-  <select id="language-switcher" v-model="locale" aria-label="Language switcher" class="bg-white">
-    <option v-for="l in locales" :key="l.code" :value="l.code" v-html="l.name" />
+  <select id="language-switcher" aria-label="Language switcher" class="bg-white" @change="onChange">
+    <option v-for="l in locales" :key="l.code" :value="l.code">
+      <NuxtLink :to="switchLocalePath(l.code)" :lang="l.code">
+        {{ l.name }}
+      </NuxtLink>
+    </option>
   </select>
 </template>
